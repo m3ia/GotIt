c6693a24f83626bfeafdc649030c3cbdcc4e2f40
Meia Natividad <meianatividad@gmail.com>
2021-05-16 18:21:12 -0700

    add domain v files for gcal api

--- server/server.mjs
+++ server/server.mjs
@@ -6,8 +6,51 @@ import * as db from "./db.mjs";
 const app = express();
 const port = process.env.PORT || 4000;
 
+// router instance for lists
+const lists = express.Router();
 // items defines the routes
 const items = express.Router();
+
+// gets all lists
+lists.get("/", async (request, response) => {
+  const lists = await db.getLists();
+  response.status(200).json(lists);
+});
+
+// gets one list
+lists.get("/:id", async (request, response) => {
+  const { id } = request.params;
+  const list = await db.getList(id);
+  // response.json(items.rows[0]);
+  response.status(200).json(list);
+});
+
+lists.use(express.json());
+
+// adds an list
+lists.post("/", async (request, response) => {
+  const { name } = request.body;
+  const list = await db.addList(name);
+  response.status(201).json(list);
+  // alternatively: response.json(newItem.rows[0]);
+  console.log("i'm in post and item is: ", list); // to test
+});
+
+// edits a list
+lists.put("/:id", async (request, response) => {
+  const list = request.body;
+  await db.updateList(list);
+  response.status(201);
+  response.json("List was updated");
+});
+
+// http req to delete a list based on id
+lists.delete("/:id", async (request, response) => {
+  const { id } = request.params;
+  await db.deleteList(id);
+  console.log("in delete", request.params);
+  response.status(200);
+});
 
 // gets all items
 items.get("/", async (request, response) => {
@@ -53,7 +96,10 @@ items.delete("/:id", async (request, response) => {
   response.status(200);
 });
 
+app.use("/api/lists", lists);
 app.use("/api/items", items);
+
+app.use(express.static("public"));
 
 process.env?.SERVE_REACT?.toLowerCase() === "true" &&
   app.use(
