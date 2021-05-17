55db1d334d04c853aea14edadd7c0017c37a88f8
Meia Natividad <meianatividad@gmail.com>
2021-05-17 12:18:56 -0700

    WIP setting up gcal api and log in page

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,54 @@
 import * as React from "react";
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 
+import gcal from "./ApiCalendar";
 import ListItems from "./ListItems";
+import ViewAllLists from "./ViewAllLists";
 // import * as apiClient from "./apiClient";
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(gcal.sign);
+
+  useEffect(() => {
+    gcal.onLoad(() => {
+      setIsAuthenticated(gcal.gapi.auth2.getAuthInstance().isSignedIn.get());
+      gcal.listenSign((sign) => setIsAuthenticated(sign));
+    });
+  }, []);
   return (
     <Fragment>
+      <Login {...{ isAuthenticated }} />
+      {isAuthenticated ? <Events /> : null}
       <div className="container">
+        <ViewAllLists />
         <ListItems />
       </div>
     </Fragment>
+  );
+};
+
+const Login = ({ isAuthenticated }) =>
+  isAuthenticated ? (
+    <button onClick={gcal.handleSignoutClick}>Log out</button>
+  ) : (
+    <button onClick={gcal.handleAuthClick}>Log in</button>
+  );
+
+const Events = () => {
+  const [events, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    gcal
+      .listUpcomingEvents(10)
+      .then(({ result: { items } }) => setEvents(items));
+  }, []);
+
+  return events.length === 0 ? null : (
+    <ul>
+      {events.map((event) => (
+        <li key={event.id}>{event.summary}</li>
+      ))}
+    </ul>
   );
 };
 
